d2418930a3c70af58237b7b9ac92391ee25c0152
hxt12345 <95265768+hxt12345@users.noreply.github.com>
2022-09-13 08:30:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,17 +23,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老0婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oLqdv554aEMV0NDWmuM5evdrhqfQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "drJc6GLKXN_UeNjVlVF9TL_JSSUbMYR4dty02oU19RQ",
       // 所在省份
-      province: "广东",
+      province: "河南",
       // 所在城市
-      city: "肇庆",
+      city: "郑州",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate:'8-28',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -58,10 +58,10 @@ export const config = {
       ]
     },
     {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
+      name: "曹硕",
+      id: "oLqdv554aEMV0NDWmuM5evdrhqfQ",
+      useTemplateId: "	drJc6GLKXN_UeNjVlVF9TL_JSSUbMYR4dty02oU19RQ",
+      province: "河南",
       city: "",
       horoscopeDate: '',
       horoscopeDateType: '',
@@ -102,7 +102,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "cbLKzO2AhfOt126crrA3HqvfggnCHyyNGprQv5JcmB0",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -110,7 +110,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oLqdv554aEMV0NDWmuM5evdrhqfQ",
     },
     // 你可以不断按格式往下增加
     // ...
